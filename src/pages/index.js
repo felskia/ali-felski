e4f5c039bf6c53c7d2d7983254ac7b465f35102e
Ali Felski <alifelski@gmail.com>
2018-04-28 21:20:23 -0400

    cleaned up styles thoughout on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ const IndexPage = () => (
     </section>
     <section className="section section--centered">
       <div className="row">
-        <h2 className="header--xlarge">I can't help wanting to learn</h2>
-        <p>I love being creative and constantly learning new things. From redesigning my home to learning about new cultures, I love doing and experiencing it all.</p>
+        <h2 className="header--xlarge withDescription">I can't help wanting to learn</h2>
+        <p className="intro-copy">I love being creative and constantly learning new things. From redesigning my home to learning about new cultures, I love doing and experiencing it all.</p>
         <Link className="button" to="/page-2/">Learn More</Link>
       </div>
     </section>
